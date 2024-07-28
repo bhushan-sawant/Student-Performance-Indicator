c2f73eff879ed0a2d756ed6ce937202cd1c9bfc6
bhushan-sawant <bhushansawant115@gmail.com>
2024-07-28 15:23:44 +0530

    Model Trainer Done

--- src/components/main.py
+++ src/components/main.py
@@ -1,10 +1,16 @@
 from src.components.data_ingestion import DataIngestion
 from src.components.data_transformation import DataTransformation
-
+from src.components.model_trainer import ModelTrainer
 
 if __name__ == "__main__":
     obj = DataIngestion()
     train_data, test_data = obj.initiate_data_ingestion()
 
     data_transformation = DataTransformation()
-    data_transformation.initiate_data_transformation(train_data, test_data)
+    # To create pkl file for data transformation
+    #data_transformation.initiate_data_transformation(train_data, test_data)
+    
+    train_arr, test_arr, _ = data_transformation.initiate_data_transformation(train_data, test_data)
+
+    modeltrainer = ModelTrainer()
+    print(modeltrainer.initiate_model_trainer(train_arr, test_arr))
